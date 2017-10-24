d009cb8c2753dcf2790f78334bfce1bb45033824
Alvixeon <32423825+Alvixeon@users.noreply.github.com>
2017-10-23 19:25:55 -0700

    Add files via upload

---
+++ urban.py
@@ -0,0 +1,47 @@
+#imports
+import requests,lxml
+from lxml import html
+#end imports
+
+#globals
+global trm
+global finalurl
+#end globals
+
+def define(trm):
+	trm = parseTerm(trm)
+	finalurl = "https://www.urbandictionary.com/define.php?term=" + trm
+	pageContent=requests.get(finalurl)
+	tree = html.fromstring(pageContent.content)
+	final = tree.xpath('//*[@id="content"]/div[1]/div[3]//text()')
+	
+	if "Define it!" in final:
+		print ("[*] This term does not appear to exist on the urbandictionary site, please try a different string")
+	else:
+		finstr = ''.join(final)
+		#print (trm + " - " + finstr) debug testing
+		return finstr
+
+def example (ex):
+	ex = parseTerm(ex)
+	finalurl = "https://www.urbandictionary.com/define.php?term=" + ex
+	pageContent=requests.get(finalurl)
+	tree = html.fromstring(pageContent.content)
+	final = tree.xpath('//*[@id="content"]/div[1]/div[4]//text()')
+		
+	if "Define it!" in final:
+		print ("[*] This term does not appear to exist on the urbandictionary site, please try a different string")
+	else:
+		finstr = ''.join(final)
+		#print ("Ex: " + finstr) debug testing
+		return finstr
+	
+	
+def parseTerm(trm):
+	trm = trm.replace(" " , "+")
+	return trm
+
+
+#if __name__ == "__main__": #moreeee debug testing
+	#define("cow")
+	#example("anime")
